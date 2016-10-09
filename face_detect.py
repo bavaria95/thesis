b9d87e659e73dce4f1e563369dbd3fb324931991
Dima Petruk <bavaria95@gmail.com>
2016-10-09 18:16:58 +0300

    scaling image

--- face_detect.py
+++ face_detect.py
@@ -1,31 +1,41 @@
 import cv2
 import sys
+import numpy as np
 
-# Get user supplied values
-imagePath = sys.argv[1]
-cascPath = sys.argv[2]
+# to what size resize image
+N = 96
 
 # Create the haar cascade
-faceCascade = cv2.CascadeClassifier(cascPath)
+faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+
+def extract_faces(img):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    new_faces = []
+
+    # Detect faces in the image
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+    )
+
+    # resize images
+    for (x, y, w, h) in faces:
+        face = img[y:y + h, x:x + w]
+        face = cv2.resize(face, (N, N), interpolation=cv2.INTER_LANCZOS4)
+
+        new_faces.append(face)
+
+    return new_faces
+
+# Get user supplied values
+imagePath = '1.png'
 
 # Read the image
 image = cv2.imread(imagePath)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# Detect faces in the image
-faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.1,
-    minNeighbors=5,
-    minSize=(30, 30),
-    flags = cv2.cv.CV_HAAR_SCALE_IMAGE
-)
-
-print("Found {0} faces!".format(len(faces)))
-
-# Draw a rectangle around the faces
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-cv2.imshow("Faces found", image)
+f = extract_faces(image)
+cv2.imshow('', f[0])
 cv2.waitKey(0)
