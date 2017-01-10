0f335489bd72ccacae55a90a800294f3fd5bdd22
Dima Petruk <bavaria95@gmail.com>
2017-01-10 19:35:01 +0200

    Simple refactoring

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import net_training as nt
 
-datadir = "D:\\inzynierka\\data\\"
-imagedir = "cohn-kanade-images\\"
-labeldir = "Emotion\\"
+# datadir = "D:\\inzynierka\\data\\"
+# imagedir = "cohn-kanade-images\\"
+# labeldir = "Emotion\\"
 
-network = nt.build_cnn('model.npz')
+network = nt.build_cnn('models/model.npz')
 
 # nt.train_net(datadir, imagedir, labeldir, network)
 
-faces = nt.load_img("anger.jpg")
+faces = nt.load_img("images/anger.jpg")
 tab = nt.evaluate(network, faces)
-print ("anger = {:.2f}%\ncontempt = {:.2f}%\ndisgust = {:.2f}%\nfear = {:.2f}%\nhappy = {:.2f}%\nsadness = {:.2f}%\nsurprise = {:.2f}%\n".format(tab[0][0]*100,tab[0][1]*100,tab[0][2]*100,tab[0][3]*100,tab[0][4]*100,tab[0][5]*100,tab[0][6]*100))
-
+print("anger = {:.2f}%\ncontempt = {:.2f}%\ndisgust = {:.2f}%\nfear = {:.2f}%\nhappy = {:.2f}%\nsadness = {:.2f}%\nsurprise = {:.2f}%".format(*tab[0]*100))
